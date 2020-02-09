ded3edbbdc432fca383b55a791680b37e675c297
Is0top <yurakh@gmail.com>
2020-02-09 21:01:54 +0300

    BeginPlay added

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,6 +3,20 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Player controller begin play"));
+	ATank* posessedTank = GetControlledTank();
+	if (posessedTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *posessedTank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No tank posessed"));
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
